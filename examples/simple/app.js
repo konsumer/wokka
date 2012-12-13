fc053a22d9f908df4b1be210fb7f2a0d279e6812
David Konsumer <konsumer@jetboystudio.com>
2012-12-13 07:19:59 -0800

    push works

--- examples/simple/app.js
+++ examples/simple/app.js
@@ -1,5 +1,48 @@
-exports.attachments = 'pub';
+var wokka = require('../../../wokka'), path = require('path');
 
-exports.views = {
-
+var app = {
+    "_id": '_design/app',
+    "rewrites": [{
+        "from": "/",
+        "to": 'index.html'
+    }, {
+        "from": "/api",
+        "to": '../../'
+    }, {
+        "from": "/api/*",
+        "to": '../../*'
+    }, {
+        "from": "/*",
+        "to": '*'
+    }]
 };
+
+app.views = {
+    "getByType" : {
+        "map": function (doc) {
+            if (!doc.type) return;
+            emit(doc.type, null);
+        }
+    },
+    "getByTypeFull" : {
+        "map": function (doc) {
+            if (!doc.type) return;
+            emit(doc.type, doc);
+        }
+    }
+};
+
+app.validate_doc_update = function(newDoc, oldDoc, userCtx) {
+    if(newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
+        throw "Only admin can delete documents on this database.";
+    }
+};
+
+wokka.attach(path.join(__dirname, 'pub'), function(attachments){
+    app['_attachments'] = attachments;
+});
+
+// for a super simple index.html + app file:
+// wokka.attach(path.join(__dirname, 'index.html'), function(attachments){ app['_attachments'] = attachments; });
+
+module.exports = app;
--- main.js
+++ main.js
@@ -5,55 +5,121 @@
 
 // fancy dep-check
 try{
-    var fs = require('fs'), path=require('path'), ugly=require('uglify-js'), toSource = require('tosource');
+    var url = require('url'),
+        fs = require('fs'),
+        path=require('path'),
+        _ = require('cloneextend'),
+        wrench = require("wrench"),
+        cradle = require('cradle'),
+        mime = require('mime');
 }catch(e){
     console.log("Install wokka dependencies with 'npm install'.");
     process.exit(1);
 }
 
 /**
- * Turn code-strings & any javascript thing into a minified chunk of code
- * @param  {Object} obj     String, or any other javascript object. Strings become the body of a function.
- * @param  {Object} options uglify gen_code options + mangle & squeeze
- * @return {String}         minified code
+ * local: get couchdb instance
+ * @param  {[type]}   dburl    couchdb URL
+ * @param  {Function} callback called when db is setup: function(err, db)
  */
-function minify(obj, options){
-    options = options || {"mangle":false, "squeeze":true};
-    if (typeof obj == "string") obj = new Function("", obj);
-    var jsp = ugly.parser;
-    var pro = ugly.uglify;
-    var ast = jsp.parse(toSource(obj));
-
-    if (options.mangle) ast = pro.ast_mangle(ast);
-    if (options.squeeze) ast = pro.ast_squeeze(ast);
-
-    return pro.gen_code(ast, options);
+function getdb(dburl, callback){
+    var u = url.parse(dburl);
+    var db = new(cradle.Connection)(u.protocol + '//' + u.hostname, u.port).database(u.path.slice(1));
+    db.exists(function (err, exists) {
+        if (!exists){
+            db.create(function(res){
+                callback(err, db);
+            });
+        }else{
+            callback(err, db);
+        }
+    });
 }
 
 /**
- * Push code & attachments into couchdb
- * @param  {Object} app   the app object
- * @param  {String} dburl The couch URL
- * @param  {String} cwd   directory to operate in
+ * Given a dir-name, get all attachments in couchdb format
+ * @param  {String}   dir      location of attachment files
+ * @param  {Function} callback called when all attachments are loaded: function(attachments)
  */
-exports.push = function(app, dburl, cwd){
-    console.log(minify(app));
+exports.attach = function(dir, callback){
+    var attachments = {};
+
+    var worker = function(name, file, icallback){
+        fs.readFile(file, function(err, body){
+            attachments[name] = {
+                'content_type': mime.lookup(path.join(dir, file)),
+                'data': body.toString('base64')
+            };
+            icallback();
+        });
+    };
+
+    fs.stat(dir, function(err, stats){
+        if (err) throw(err);
+        if (stats.isDirectory()){
+            var files = wrench.readdirSyncRecursive(dir);
+            var i = 0;
+            files.forEach(function(file){
+                fs.stat(path.join(dir, file), function(err, stats){
+                    if (stats.isFile() && path.basename(file)[0] != '.'){
+                        worker(file, path.join(dir, file), function(){
+                            i++;
+                            if (i == files.length){
+                                callback(attachments);
+                            }
+                        });
+                    }else{
+                        i++;
+                        if (i == files.length){
+                            callback(attachments);
+                        }
+                    }
+                });
+            });
+        }else{
+            worker(path.basename(dir), dir, function(){
+                callback(attachments);
+            });
+        }
+    });
+};
+
+/**
+ * Push app into couchdb
+ * @param  {Object} app   the app object
+ * @param  {String} dburl the couch URL
+ */
+exports.push = function(app, dburl){
+    var db = getdb(dburl, function(err, db){
+        if (err) throw(err);
+        var doc = _.extend({}, app);
+        db.save(app['_id'], doc, function(err, res){
+            if (err) throw(err.error + ' : ' + err.reason);
+        });
+    });
 };
 
 /**
  * Pull code & attachments from couchdb
  * @param  {Object} app   the app object
- * @param  {String} dburl The couch URL
+ * @param  {String} dburl the couch URL
  * @param  {String} cwd   directory to operate in
  */
 exports.pull = function(app, dburl, cwd){
-    console.log(arguments);
+    var db = getdb(dburl, function(err, db){
+        if (err) throw(err);
+        db.get(app['_id'], function(err, doc){
+            attachments = _.extend({}, doc['_attachments']);
+            delete doc['_attachments'];
+            console.log(doc);
+        });
+    });
 };
 
 /**
  * Push code & attachments into couchdb, watch for changes
  * @param  {Object} app   the app object
- * @param  {String} dburl The couch URL
+ * @param  {String} dburl the couch URL
  * @param  {String} cwd   directory to operate in
  */
 exports.watch = function(app, dburl, cwd){
